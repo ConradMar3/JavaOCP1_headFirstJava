7338ef625974bd5e1f2e7e251de826deed46b7f3
ConradMar3 <conradmare890317@gmail.com>
2022-01-10 07:35:43 -0800

    2022/01/10 (pm)
    Chapter2 - dot.Operator

---
+++ Chapter1/Excersises/Shuffle1.java
@@ -0,0 +1,24 @@
+package Excersises;
+
+class Shuffle1 {
+    public static void main(String[] args) {
+        int x = 3;
+
+        x = x - 1;
+        System.out.print("~");
+
+        while (x > 0) {
+
+            if (x > 2) {
+                System.out.print("a");
+            }
+            if (x == 2) {
+                System.out.print(" b c ");
+            }
+            if (x == 1) {
+                System.out.print(" d ");
+                x = x - 1;
+            }
+            }
+        }
+    }
